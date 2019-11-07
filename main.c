930c5f1bbff8cbd1753bd30fda19bfaf1447c3c9
bshumin <bshumin@users.noreply.github.com>
2019-11-07 12:10:07 -0500

    Added main function and minor main.c changes

--- main.c
+++ main.c
@@ -5,45 +5,49 @@
 
  initialize queues
  */
+// FIXME added main function, not sure if correct
+int main(void){
+  /* malloc new EV_ARRIVE event and passenger */
+  event_t *start_ev;
+  start_ev = event_create();
+  start_ev.passenger = (passenger_t *)malloc(sizeof(passenger_t));
 
+  /* schedule EV_ARRIVE event at t=0 and put in event queue */
+  start_ev.event_time = 0.0;
+  event_schedule(start_ev);
 
-/* malloc new EV_ARRIVE event and passenger */
-event_t *start_ev;
-start_ev = event_create();
-start_ev.passenger = (passenger_t *)malloc(sizeof(passenger_t));
-
-/* schedule EV_ARRIVE event at t=0 and put in event queue */
-start_ev.event_time = 0.0;
-event_schedule(start_ev);
-
-/* run main loop */
-while(!event_empty(eq))
-{
-    new_ev = event_cause();
-    time_set(new_ev.event_time);
-    switch (new_ev.event_type)
-    {
-    case (EV_ARRIVE) :
-        new_ev.passenger.arrive_time = time_get();
-        /* create EV_ENQUEUE event for this passenger */
-        /* schedule EV_ENQUEUE event */
-        if (MAX_PASS > num_passengers++)
-        {
-            /* create new EV_ARRIVE event and passsenger */
-            /* schedule EV_ARRIVE event */
-        }
-        break;
-    case (EV_AIRLINEQ) :
-        break;
-    case (EV_AIRLINE) :
-        break;
-    case (EV_IDQ) :
-        break;
-    /* remaining event types */
-    default :
-        /* error */
-        break;
-    }
-    /* free event */
+  /* run main loop */
+  while(!event_empty(eq)) //FIXME where does eq come from ?!?!
+  {
+      new_ev = event_cause();
+      time_set(new_ev.event_time);
+      switch (new_ev.event_type)
+      {
+      case (EV_ARRIVE) :
+          new_ev.passenger.arrive_time = time_get();
+          /* create EV_ENQUEUE event for this passenger */
+          /* schedule EV_ENQUEUE event */
+          if (MAX_PASS > num_passengers++)
+          {
+              /* create new EV_ARRIVE event and passsenger */
+              /* schedule EV_ARRIVE event */
+          }
+          break;
+      case (EV_AIRLINEQ) :
+          break;
+      case (EV_AIRLINE) :
+          break;
+      case (EV_IDQ) :
+          break;
+      /* remaining event types */
+      default :
+          /* error */
+          fprintf(stderr, "Something went wrong.")
+          //return -1;
+          break;
+      }
+      /* free event */
+  }
+  /* Print overall stats */
+  return 0;
 }
-/* Print overall stats */
