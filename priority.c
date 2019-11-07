a1c889bc8248c1d75b5c94de10baba9a64c53c18
bshumin <bshumin@users.noreply.github.com>
2019-11-07 11:46:52 -0500

    added some heap functions

--- priority.c
+++ priority.c
@@ -4,3 +4,42 @@
 /* Arr[(i-1)/2]	Returns the parent node     */
 /* Arr[(2*i)+1]	Returns the left child node */
 /* Arr[(2*i)+2]	Returns the right child node*/
+
+/*
+  TODO
+  figure out how/if this works
+*/
+
+int * make_PQ(void) {
+  int heap[];
+
+}
+
+/*
+  TODO
+  code from online to max heapify, check if correct
+*/
+int * max_heapify(int Arr[], int i, int n)
+{
+    int left = 2*i;                //left child
+    int right = 2*i + 1;           //right child
+    int largest;
+    int temp;
+
+    if(left<= n & Arr[left] > Arr[i] )
+          largest = left;
+    else
+         largest = i;
+    if(right <= n and Arr[right] > Arr[largest] )
+        largest = right;
+    if(largest != i )
+    {
+        // swap Arr[i] and Arr[largest]
+        temp = Arr[i];
+        Arr[i] = Arr[largest];
+        Arr[largest] = temp;
+
+        Arr = max_heapify(Arr, largest,n);
+    }
+    return Arr;
+ }
