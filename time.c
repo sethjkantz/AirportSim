daba695b7c2226e9c57846188f20c90fa2083712
bshumin <bshumin@users.noreply.github.com>
2019-11-08 15:32:06 -0500

    time.c functions added

--- time.c
+++ time.c
@@ -2,3 +2,36 @@
 static double sim_time;
 //delta time
 //current time
+void time_init(){
+
+}
+
+void time_fini(){
+
+}
+
+double time_get(){
+
+}
+
+void time_set(double t){
+
+}
+double time_arrive(){
+
+}
+double time_airline(){
+
+}
+double time_id(){
+
+}
+double time_scan(){
+
+}
+double time_train(){
+
+}
+double time_gate(){
+  
+}
