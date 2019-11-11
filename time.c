2a1ce9aab1756a3349786952fd81abbd8e2d8650
sethjkantz <skantz@clemson.edu>
2019-11-11 15:06:06 -0500

    return 0 for blank functions

--- time.c
+++ time.c
@@ -18,26 +18,32 @@ void time_set(double t){
   sim_time = t;
 }
 
+// returns arrival time
 double time_arrive(){
 
 }
 
+// blank
 double time_airline(){
-
+  return 0;
 }
 
+// blank
 double time_id(){
-
+  return 0;
 }
 
+// blank
 double time_scan(){
-
+  return 0;
 }
 
+// blank
 double time_train(){
-
+  return 0;
 }
 
+// blank
 double time_gate(){
-
+  return 0;
 }
