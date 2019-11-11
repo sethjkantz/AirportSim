659af1ce79e148fdd165fc80e62df19d442d6857
sethjkantz <skantz@clemson.edu>
2019-11-11 16:39:00 -0500

    comments

--- priority.c
+++ priority.c
@@ -1,9 +1,9 @@
 /* PQ implemented as a heap   */
 /* also called the event queue*/
 
-/* Arr[(i-1)/2]	Returns the parent node     */
-/* Arr[(2*i)+1]	Returns the left child node */
-/* Arr[(2*i)+2]	Returns the right child node*/
+/* Arr[(i]	Returns the parent node     */
+/* Arr[(2*i)]	Returns the left child node */
+/* Arr[(2*i)+1]	Returns the right child node*/
 
 /*
   TODO
