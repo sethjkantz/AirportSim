07231d7cdc6e285fbc77d5524493e0b63ae76dd6
sethjkantz <skantz@clemson.edu>
2019-11-12 20:53:11 -0500

    basis for event functions

--- event.c
+++ event.c
@@ -1,26 +1,33 @@
-/* events and event queue (PQ) */
-//calls priority to create an event PQ
+/* 
+   event.c
 
-// //event_time takes in event type and returns the time
-// double event_time(event_t *ev){
-//   return ev->time;
-// }
+   - events and event queue (PQ) 
+   - calls priority to create an event PQ
+
+   ?-
+   - is eq a global pq?
+   - is init doing anything?
+   - is any of this doing anythin?
+   - why did we choose this major?
+*/
+
 #include "defs.h"
+
+
 /* initializes events, creates a priority queue */
+/* should this be a void void? / is anything happening in this function
+   ? - SJK
+*/
 void event_init(void){
-  int size;
-  size = MAX_PASS;
-  priority_t Events = priority_init(size);
+  priority_t Events = priority_init(MAX_PASS);
 }
 
 /* frees up all event space, including space in the priority
    queue */
 void event_fini(event_t *ev){
-  qNode *rov;
   if(ev != NULL){
     queue_finalize(&(ev->queue));
-  
-  
+    free(ev->passenger);
     free(ev);
   }
 }
@@ -32,15 +39,20 @@ event_t *event_create(void){
   return new_ev;
 }
 
+/* removes and frees top event*/
 void event_destroy(event_t *ev){
-
+  ev = priority_remove(eq);
+  event_fini(ev);
+  
 }
 
+/* inserts ev into pq */
 void event_schedule(event_t *ev){
-
+  priority_insert(eq,ev);
 }
 
 /* pops top off */
+/* Is eq a global ? - SJK*/
 event_t *event_cause(void){
   event_t * ev = priority_remove(eq);
   return ev;
